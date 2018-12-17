b6b2a5679c585f9bc120645691e3f4b068c9d20a
ernestpauliucenko <45064411+ernestpauliucenko@users.noreply.github.com>
2018-12-17 04:22:10 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,5 @@
-print("Ren & Stimpy")
+def index(request):
+    r = requests.get('http://httpbin.org/status/418')
+    print(r.text)
+    return HttpResponse('<pre>' + r.text + '</pre>')
+index(request)
